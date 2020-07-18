4443f3fc7a6ecfda5a933aa8c680de393a4d58e4
Teema Luksameset <tm@odds.team>
2020-07-18 10:43:44 +0700

    FizzBuzz still pass hahahaha :trollface:

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,6 +1,8 @@
 import unittest
 
 def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return 'FizzBuzz'
     if number % 3 == 0:
         return 'Fizz'
 
@@ -12,5 +14,13 @@ class TestFizzBuzz(unittest.TestCase):
     def test_input_6_should_get_fizz(self):
         result = fizzbuzz(6)
         self.assertEqual(result, 'Fizz')
+
+    def test_input_15_should_get_fizz_buzz(self):
+        result = fizzbuzz(15)
+        self.assertEqual(result, 'FizzBuzz')
+
+    def test_input_450_should_get_fizz_buzz(self):
+        result = fizzbuzz(450)
+        self.assertEqual(result, 'FizzBuzz')
 
 unittest.main()
