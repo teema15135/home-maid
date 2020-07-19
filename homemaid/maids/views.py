ec09cea5d7b50e63896a4b645450d2978c4b207a
Teema Luksameset <tm@odds.team>
2020-07-19 10:42:04 +0700

    Use template engine instead of concat string :trollface:

--- homemaid/maids/views.py
+++ homemaid/maids/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 
@@ -6,12 +6,13 @@ from .models import Maid
 
 
 class MaidListView(View):
+    template_name = 'maid_list.html'
+    
     def get(self, request):
-        html = ''
-        maids = Maid.objects.all()
-        for maid in maids:
-            html += f'<li>{maid.name}</li>'
-        return HttpResponse(html)
+        context = {
+            'maid_list': Maid.objects.all()
+        }
+        return render(request, self.template_name, context)
 
 def maid_another_list_view(request):
     print(dir(request))
